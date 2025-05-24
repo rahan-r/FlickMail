b693ad50a423036ed88cc68f2343e6b4d0c05bd1
rahan <rahan.vadakkethil@gmail.com>
2025-05-24 17:07:13 +0530

    Update SERVER_URL.js

--- flick-mail/src/service/SERVER_URL.js
+++ flick-mail/src/service/SERVER_URL.js
@@ -1,1 +1,1 @@
-export const server_url = import.meta.env.REACT_APP_SERVER_URL || "http://localhost:3000/api";
+export const server_url = import.meta.env.REACT_APP_SERVER_URL || "https://rahan.helioho.st/api";
